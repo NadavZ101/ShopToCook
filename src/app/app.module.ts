5ced279cbc79d94bf0a013640cb8903d7e68c5d4
NadavZ101 <nadavzivyon@gmail.com>
2024-05-06 18:05:42 +0300

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
+import { ShoppingListComponent } from './cmps/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './cmps/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './cmps/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './cmps/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './cmps/recipe-details/recipe-details.component';
+import { IngredientComponent } from './cmps/ingredient/ingredient.component';
+import { RecipeComponent } from './cmps/recipe/recipe.component';
+import { HeaderComponent } from './cmps/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    IngredientComponent,
+    RecipeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cmps/ingredient/ingredient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ingredient',
+  templateUrl: './ingredient.component.html',
+  styleUrl: './ingredient.component.css'
+})
+export class IngredientComponent {
+
+}
